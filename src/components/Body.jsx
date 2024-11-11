be4f3e670a0e5ff379ecb226b8444fd1b87f3182
Jean <jeanfsole@hotmail.com>
2024-11-11 12:39:35 +0100

    Cambios minimos de prueba de funcionamiento
    Co-authored-by: Agustin Rossello <RosselloAgustin@users.noreply.github.com>

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='flex justify-center bg-verde0'>
+
+        <button className="p-2 m-2 bg-verde8 text-white rounded-lg	">Finanzas</button>
+        <button className="p-2 m-2 bg-verde8 text-white rounded-lg	">Lista</button>
+        <button className="p-2 m-2 bg-verde8 text-white rounded-lg	">Calendario</button>
+
+    </div>
+  )
+}
+
+export default Body
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+/* Cuando edites esto, tenes que cerrar y el IDE y abrirlo de nuevo para que se actualice */
 export default {
   content: ["./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend:{
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          verde0: '#d6ffc7',
+          verde1: '#b1ff96',
+          verde2: '#7ffd59',
+          verde3: '#53f427',
+          verde4: '#31db07',
+          verde5: '#21af01',
+          verde6: '#1a7f06',
+          verde7: '#1b680c',
+          verde8: '#19580f',
+          verde9: '#073102',
+        },
+      },
   },
   plugins: [],
 }
-
-
-
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
